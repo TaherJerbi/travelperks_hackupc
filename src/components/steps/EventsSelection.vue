2fd1cd2baef0d90091a8f284f0a49205b7e55445
Taher Jerbi <jerbi.taherr@gmail.com>
2024-05-07 00:42:34 +0200

    last touches

--- src/components/steps/EventsSelection.vue
+++ src/components/steps/EventsSelection.vue
@@ -5,23 +5,48 @@
     <div
       v-for="event in props.events"
       :key="event.id"
-      class="shadow-5 my-2 border-round-xl surface-100 flex flex-column align-items-stretch gap-2"
+      class="shadow-5 my-4 border-round-xl surface-100 flex flex-column align-items-stretch gap-2 border-round-lg overflow-hidden relative"
+      @click="toggleEvent(event)"
+      :class="{
+        'shadow-6 bg-green-100': selectedEvents.includes(event.id),
+      }"
     >
+      <Tag class="absolute m-2" severity="warning">
+        <i class="pi pi-user" /><span class="text-sm pl-2">{{
+          Math.floor(Math.random() * 100)
+        }}</span>
+      </Tag>
       <img :src="event.images[0].url" alt="Event image" height="200" />
       <div class="px-4">
         <p class="font-semibold">{{ event.name }}</p>
-        <p class="text-500 mt-5">{{ event.dates.start.localDate }}</p>
+        <div class="flex justify-content-between">
+          <p class="text-500">{{ event._embedded.venues[0].name }}</p>
+          <p class="text-500">{{ event.dates.start.localDate }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { TicketmasterEvent } from "../../data/ticketmaster";
+import Tag from "primevue/tag";
+import PrimeButton from "primevue/button";
 
 const props = defineProps<{
   events: TicketmasterEvent[];
 }>();
+
+const selectedEvents = ref<string[]>([]);
+
+function toggleEvent(event: TicketmasterEvent) {
+  if (selectedEvents.value.includes(event.id)) {
+    selectedEvents.value = selectedEvents.value.filter((id) => id !== event.id);
+  } else {
+    selectedEvents.value.push(event.id);
+  }
+}
 </script>
 
 <style scoped>
